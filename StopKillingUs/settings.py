990365d95801951b7c26e8e2f90efb7f224077a9
tiana-horn <tiana.horn@gmail.com>
2020-05-31 22:59:51 -0400

    Move static folder

--- StopKillingUs/settings.py
+++ StopKillingUs/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-import django_heroku
 import os
+import django_heroku
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -128,7 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
