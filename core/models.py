098dc89db2ff2755b51431412ff297249c127d73
tiana-horn <tiana.horn@gmail.com>
2020-06-10 23:54:33 -0400

    Add Post

--- core/models.py
+++ core/models.py
@@ -4,4 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     username = models.CharField(max_length=33, unique=True)
 
-
+class Post(models.Model):
+    message = models.CharField(max_length=777)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
--- core/views.py
+++ core/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
-from core.models import User
+from core.models import User, Post
 
 
 def index(request):
+    posts = Post.objects.all()
     return render(request, 'index.html', {
         'google_map_key': settings.GOOGLE_MAP_KEY,
-
+        'posts': posts,
     })
 
 def privacyPolicy(request):
